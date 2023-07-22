29f747e847fae8e911aa32467c5c33834bc77a4f
mitul-goswami <126082561+mitul-goswami@users.noreply.github.com>
2023-07-22 13:38:15 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,7 +6,7 @@
  */
 
 import React from "react";
-import arrowSvg from "../images/down-arrow.svg";
+import arrowSvg from "src/images/pexels-felix-mittermeier-956999.jpg";
 import PropTypes from "prop-types";
 
 /**
