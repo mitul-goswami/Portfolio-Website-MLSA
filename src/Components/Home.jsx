4ee9102938c3d0fa98983cac3f14f8db9b3b8f92
mitul-goswami <126082561+mitul-goswami@users.noreply.github.com>
2023-07-22 15:44:23 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/pexels-felix-mittermeier-956999.jpg";
+import image from "../images/lighthouse-7917690.jpg";
 
 const imageAltText = "Ath Microsoft Surface Pen";
 
